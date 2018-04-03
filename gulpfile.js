8aaf2ea0fd66df376fed102e639e244c510d95fb
Filip Lindbladh <filiplindbladhh@gmail.com>
2018-04-03 13:34:15 +0200

    added namespacing and added --production flag to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,22 @@ var rename = require('gulp-rename')
 var cleanCSS = require('gulp-clean-css')
 var babel = require('gulp-babel')
 var eslint = require('gulp-eslint')
+var sourcemaps = require('gulp-sourcemaps')
 var browserSync = require('browser-sync').create()
 var reload = browserSync.reload
 var config = require('./config.js')
+var gulpif = require('gulp-if')
 
 gulp.task('js', function () {
   return gulp.src(['./src/js/*.js'])
+    .pipe(gulpif(!config.production, sourcemaps.init()))
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(babel())
     .pipe(concat('app.js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
+    .pipe(gulpif(!config.production, sourcemaps.write('.')))
     .pipe(gulp.dest('./dist/js/'))
 })
 gulp.task('sass', function () {
@@ -44,14 +48,14 @@ gulp.task('editor-styles', function () {
     .pipe(gulp.dest('./dist/css/'))
 })
 gulp.task('watch', function () {
-  browserSync.init({
-    files: ['./**/*.php'],
-    proxy: config.proxy
-  })
-  gulp.watch('./src/sass/**/*.scss', ['sass', reload])
-  gulp.watch('./src/js/*.js', ['js', reload])
-  gulp.watch('./src/sass/style-assets/_typography.scss', ['editor-styles', reload])
+  if (!config.production) {
+    browserSync.init({
+      files: ['./**/*.php'],
+      proxy: config.proxy
+    })
+    gulp.watch('./src/sass/**/*.scss', ['sass', reload])
+    gulp.watch('./src/js/*.js', ['js', reload])
+    gulp.watch('./src/sass/style-assets/_typography.scss', ['editor-styles', reload])
+  }
 })
 gulp.task('default', ['sass', 'js', 'watch', 'editor-styles'])
-
-gulp.task('production', ['sass', 'js', 'editor-styles'])
